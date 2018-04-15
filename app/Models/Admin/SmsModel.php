0237308ae2ed41ff6bafde33c39188fa54786ba7
ly2513 <626375290@qq.com>
2018-04-15 15:50:19 +0800

    添加model

---
+++ app/Models/Admin/SmsModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: yongli
+ * Date: 17/8/7
+ * Time: 11:35
+ * Email: yong.li@szypwl.com
+ * Copyright: 深圳优品未来科技有限公司
+ */
+namespace Admin;
+
+use YP\Core\YP_Model as Model;
+
+/**
+ * Class SmsModel
+ *
+ * @package Goods
+ */
+class SmsModel extends Model
+{
+    protected $table = 'ams_sys_sms';
+}
