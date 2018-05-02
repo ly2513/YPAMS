dad594bf1038b93e1614b9234f867557a5e38756
ly2513 <626375290@qq.com>
2018-05-02 09:08:16 +0800

    首页显示

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,11 +8,9 @@
  */
 namespace App\Controllers;
 
-use App\Libraries\SensitiveFilter;
+use Admin\UserModel;
 use YP\Core\YP_Controller as Controller;
-use YP\Config\Services;
-use YP\Libraries\Thrift\YP_ThriftClient;
-use Illuminate\Database\Capsule\Manager as DB;
+use Elasticsearch\ClientBuilder;
 
 /**
  * 框架默认控制器
@@ -28,21 +26,43 @@ class Home extends Controller
      */
     public function index()
     {
-        //        $XHPROF_ROOT  = dirname(ROOT_PATH) . '/xhprof/xhprof_lib/utils/';
-        $time         = microtime(true) * 1000;
-        $elapsed_time = number_format(($time - START_TIME), 0);
-        $this->assign('view_path', 'app/Views/' . $this->controller . '/' . $this->method . $this->extension);
-        $this->assign('controller_path', 'app/Controller/' . $this->controller . '.php');
-        $this->assign('evn', ENVIRONMENT);
-        $this->assign('elapsed_time', $elapsed_time);
-        $this->assign('version', VERSION);
-        //        $this->assign('doc_url', 'https://ly2513.gitbooks.io/youpin/content/');
-        //        $xhprof_data = xhprof_disable();
-        //        include_once $XHPROF_ROOT . "xhprof_lib.php";
-        //        include_once $XHPROF_ROOT . "xhprof_runs.php";
-        //        $xhprof_runs = new \XHProfRuns_Default();
-        //        $run_id      = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
         $this->display();
     }
 
+    public function testElasticsearch()
+    {
+        $client   = ClientBuilder::create()->build();
+        $userData = UserModel::select(['id', 'pid', 'name', 'nickname', 'phone'])->get()->toArray();
+        foreach ($userData as $key => $value) {
+            $params          = [];
+            $params['body']  = [
+                'id'       => $value['id'],
+                'pid'      => $value['pid'],
+                'name'     => $value['name'],
+                'nickname' => $value['nickname'],
+                'phone'    => $value['phone'],
+            ];
+            $params['index'] = 'emp_index';
+            $params['type']  = 'emp_type';
+            //Document will be indexed to log_index/log_type/autogenerate_id
+            $status = $client->index($params);
+            P($status);
+        }
+        echo 'create index done!';
+    }
+
+    public function search()
+    {
+        $para                                   = $this->request->getGet();
+        $client                                   = ClientBuilder::create()->build();
+        $params                                   = [];
+        $params['index']                          = 'emp_index';
+        $params['type']                           = 'emp_type';
+        $params['body']['query']['match']['name'] = $para['q'];
+        $params['body']['sort']                   = ['id' => ['order' => 'desc']];
+        $params['size']                           = 3;
+        $params['from']                           = 1;
+        $rtn                                      = $client->search($params);
+        P($rtn);
+    }
 }
